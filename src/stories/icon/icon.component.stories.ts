492376e707fec85f0474e4b44d54629e0ad56da1
marwan <marouanebenmhand2@gmail.com>
2024-07-05 15:15:01 +0100

    added social media button

--- src/stories/icon/icon.component.stories.ts
+++ src/stories/icon/icon.component.stories.ts
@@ -1,27 +1,50 @@
 import type { Meta, StoryObj } from '@storybook/angular';
-
 import { IconComponent } from './icon.component';
 
 const meta: Meta<IconComponent> = {
   component: IconComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    iconColor: {
+      control: 'color',
+    },
+    labelColor: {
+      control: 'color',
+    },
+    iconwidth: {
+      control: 'text',
+    },
+    icon: {
+      control: 'select',
+      options: [
+        'fa-brands fa-facebook-f',
+        'fa-brands fa-twitter',
+        'fa-brands fa-instagram',
+        'fa-brands fa-linkedin-in',
+        'fa-brands fa-github',
+      ],
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<IconComponent>;
 
 export const Primary: Story = {
-  // 👇 Rename this story
-  name: 'primary',
+  name: 'Social media icons',
   args: {
     label: 'Facebook',
-    primary: true,
+    icon: 'fa-brands fa-facebook-f',
+    iconColor: '#000',
+    labelColor: '#000',
+    iconwidth: '100%',
   },
 };
-export const secondary: Story = {
-  // 👇 Rename this story
-  name: 'I am the primary',
+
+export const Secondary: Story = {
+  name: 'social media actions',
   args: {
-    label: 'Facebook',
-    primary: true,
+    label: 'Instagram',
+    icon: 'fa-brands fa-instagram',
   },
 };
--- src/stories/icon/icon.component.ts
+++ src/stories/icon/icon.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.css'],
   template: `
-<article class="facebook-comments-card">
-  <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/8b34f9e090248b629192e6c23d14bd676b1050536ae7e212f9b28e1a95e26e31?apiKey=61d204e0c12947c1a81bbc8591c17a9c&" alt="Facebook icon" class="facebook-icon" />
-  <h2 class="card-title">{{ label }}</h2>
-</article>`,
+    <a href="#" class="facebook-card" [ngStyle]="{ 'width': iconwidth}">
+      <i [ngClass]="icon" [ngStyle]="{ 'color': iconColor,}" class="facebook-icon"></i>
+      <h2 class="facebook-title" [ngStyle]="{ 'color': labelColor, }">{{ label }}</h2>
+    </a>
+  `,
 })
 export class IconComponent {
-
+  @Input() label: string = 'facebook';
+  @Input() icon: string = 'fa-brands fa-facebook-f';
+  @Input() iconColor?: string;
+  @Input() labelColor?: string;
+  @Input() iconwidth?: string;
 }
