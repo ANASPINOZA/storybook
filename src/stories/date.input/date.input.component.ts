cee2af7ce5fefd52680a23e8e35aaf5760d46652
ANASPINOZA <anas.adnan2016@gmail.com>
2024-07-07 22:59:00 +0200

    added kamal's date component

---
+++ src/stories/date.input/date.input.component.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'storybook-date-input',
+  templateUrl: './date.input.component.html',
+  styleUrls: ['./date.input.component.css']
+})
+export class DateInputComponent {
+
+date: any = '';
+isValidDate(dateStr: string) { 
+ if(dateStr.trim().length > 0){
+  const dateFormat = /^\d{4}-\d{2}-\d{2}$/;
+  if (dateFormat.test(dateStr)) {
+    const [year, month, day] = dateStr.split('-').map(Number);
+    if (year < 1930 || year > 2190) {
+     return false;
+    }
+    if (month < 1 || month > 12){
+      return false;
+    }
+   const daysInMonth = new Date(year, month, 0).getDate();
+   if (day < 1 || day > daysInMonth) {
+     return false;
+   }
+   return true;
+}
+  return false;  
+ } 
+ return null;
+}
+
+myvar = 'Invalid date';
+othervar = 'Valid date';
+
+updateDate(event: Event) {
+ const selectedDate = (event.target as HTMLInputElement).value;
+  if (selectedDate !== null) {
+   this.date = selectedDate;
+  }
+}
+}
+
+
+
+//   date: any = '';
+
+  
+//   isValidDate(dateStr: string) { 
+//    if(dateStr.trim().length > 0){
+//     const dateFormat = /^\d{4}-\d{2}-\d{2}$/;
+//     if (dateFormat.test(dateStr)) {
+//       const [year, month, day] = dateStr.split('-').map(Number);
+//       if (year < 1930 || year > 2190) {
+//        return false;
+//       }
+//       if (month < 1 || month > 12){
+//         return false;
+//       }
+//      const daysInMonth = new Date(year, month, 0).getDate();
+//      if (day < 1 || day > daysInMonth) {
+//        return false;
+//      }
+//      return true;
+//   }
+//     return false;  
+//    } 
+//    return null;
+//   }
+  
+//   myvar = 'Invalid date';
+//  othervar = 'Valid date';
+
+//  updateDate(event: Event) {
+//   const selectedDate = (event.target as HTMLInputElement).value;
+//   if (selectedDate !== null) {
+//     this.date = selectedDate;
+//   }
+//  }
